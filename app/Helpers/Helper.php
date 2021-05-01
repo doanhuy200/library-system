9e97335e8783d3d848825115805e70fe9a46a622
Doan Huy <doanhuynguyenhong@gmail.com>
2021-05-01 23:03:08 +0700

    [feature/create-view-layout] Create layout for view project

---
+++ app/Helpers/Helper.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+if (!function_exists('classActivePath')) {
+    function classActivePath($value)
+    {
+        if(!is_array($value)) {
+            return Route::currentRouteName()  == $value ? 'menu-open' : '';
+        }
+        foreach ($value as $v) {
+            if(Route::currentRouteName() == $v) return 'menu-open';
+        }
+        return '';
+    }
+}
+
+if (!function_exists('classActiveSegment')) {
+    function classActiveSegment($value)
+    {
+        if(!is_array($value)) {
+            return Route::currentRouteName() == $value ? 'active' : '';
+        }
+        foreach ($value as $v) {
+            if(Route::currentRouteName() == $v) return 'active';
+        }
+        return '';
+    }
+}
